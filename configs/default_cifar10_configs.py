82ecd7253f5b785feeb0d72d52ea09746b0badbf
alexsillwood <76120289+alexsillwood@users.noreply.github.com>
2022-05-09 12:33:00 +0100

    Update default_cifar10_configs.py

--- configs/default_cifar10_configs.py
+++ configs/default_cifar10_configs.py
@@ -11,7 +11,7 @@ def get_default_configs():
   training.log_freq = 50
   training.eval_freq = 100
   ## store additional checkpoints for preemption in cloud computing environments
-  training.snapshot_freq_for_preemption = 10000
+  training.snapshot_freq_for_preemption = 100 #00
   ## produce samples at each snapshot.
   training.snapshot_sampling = True
   training.likelihood_weighting = False
